18bddf8f76d9090f05cddd45bc015d1ded109feb
taekwondodev <taekwondodev@outlook.it>
2025-01-08 12:33:27 +0100

    Aggiunta architettura del progetto, divisione in folder

--- app/src/main/java/com/example/dietiestates25/view/MainActivity.kt
+++ app/src/main/java/com/example/dietiestates25/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.dietiestates25
+package com.example.dietiestates25.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dietiestates25.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/dietiestates25/view/RegistrazioneActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dietiestates25.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dietiestates25.R
+
+class RegistrazioneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
